90addf51c3d03cf4d95aafa75401ec492914e34d
afanneau <afanneau@student.42.fr>
2018-02-26 17:29:36 +0100

    d

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const path = require("path")
+const fs = require("fs")
+
 const app = express()
 const port = 7777
 const torrentStream = require('torrent-stream');
@@ -13,9 +15,21 @@ app.set('view engine', 'pug')
 //Middlewares
 app.use('/assets', express.static('public'))
 
-var engine = torrentStream('magnet:?xt=urn:btih:1f11e98ea4736b5bb9a5c643212c10965d25958d&dn=&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopentor.org%3A2710&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969')
+var engine = torrentStream('magnet:?xt=urn:btih:5aa6a160a7470b00e4b3d0ee824333fb7c934ca7&dn=Zenith%20(2011)%20(theora)&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopentor.org%3A2710&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969', {
+    connections: 100,
+    uploads: 10,
+    tmp: '/goinfre',
+    verify: true,
+    dht: true,
+    tracker: true,
+    trackers: [
+        'udp://tracker.openbittorrent.com:80',
+        'udp://tracker.ccc.de:80'
+    ]
+})
 
 engine.on('ready', function() {
+    console.log('rdy')
     engine.files.forEach(function(file) {
         console.log('filename:', file.name);
         var stream = file.createReadStream();
@@ -25,8 +39,45 @@ engine.on('ready', function() {
     });
 });
 
+
 app.get('/', (req, res) => {
     res.render(pages + '/index.pug')
+})
+
+app.get('/video', (req, res) => {
+    console.log('estt')
+    const file = path.resolve('/goinfre/torrent-stream/5aa6a160a7470b00e4b3d0ee824333fb7c934ca7/Zenith.Part.1.2011.Theora-VODO/Zenith.Part.1.2011.Theora-VODO.ogv')
+    console.log(file)
+    fs.stat(file, (err, stats) => {
+        console.log(stats)
+        var range = req.headers.range;
+        if (!range) {
+        //  416 Wrong range
+         return res.sendStatus(416);
+        }
+        var positions = range.replace(/bytes=/, "").split("-");
+        console.log(positions)
+        var start = parseInt(positions[0], 10);
+        // var positions = [stats.size / 2, stats.size / 2 + stats.size / 10]
+        // var start = positions[0]    
+        var total = stats.size;
+        var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+        var chunksize = (end - start) + 1;
+
+        res.writeHead(206, {
+            "Content-Range": "bytes " + start + "-" + end + "/" + total,
+            "Accept-Ranges": "bytes",
+            "Content-Length": chunksize,
+            "Content-Type": "video/webm"
+          });
+        
+        var stream = fs.createReadStream(file, { start: start, end: end })
+        .on("open", function() {
+        stream.pipe(res);
+        }).on("error", function(err) {
+        res.end(err);
+        });
+    })
 })
 
 app.listen(port, (err) => {
---
+++ public/javascripts/video.js
@@ -0,0 +1,21 @@
+// var myPlayer = videojs('my-video');
+// myPlayer.on('play', () => {
+//     console.log(myPlayer.currentTime());
+//    })
+// myPlayer.ready(function() {
+//   // set current time to 2 minutes into the video
+
+//   // get the current time, should be 120 seconds
+//   var whereYouAt = myPlayer.currentTime();
+// });
+
+// $.ajax({
+//     type: 'GET',
+//     url: '/video',
+//     beforeSend: (req) => {
+//         req.setRequestHeader('range', 'bytes=18943269-19943268')
+//     }
+// }).done((data) => {
+//     console.log('done')
+//     // myPlayer.src([{type: "video/webm", src: data}])
+// });
