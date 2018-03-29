3099528ac04089f28dd52f3a6133aec3179fb545
afanneau <afanneau@student.42.fr>
2018-03-29 16:14:51 +0200

    translate api

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
+const translate = require('google-translate-api');;
+
 const routes = require('express').Router();
 const movies = require('./movies');
-
 
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Connected!' });
 });
 
 routes.use('/movies', movies)
+
+routes.get('/translate', (req, res) => {
+  let {str, lang} = req.query;
+
+  translate(str, {to: lang}).then(ret => {
+    console.log(ret.text);
+    //=> I speak English
+    console.log(ret.from.language.iso);
+    //=> nl
+    res.send(ret.text);
+  }).catch(err => {
+      console.error(err);
+      res.send();
+  });
+})
 
 
 
